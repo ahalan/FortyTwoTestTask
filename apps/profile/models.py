6ac2bf11fb192ddb8cfc8d9d80a50ce797a8fb66
Andrey Halan <halan.andrey@gmail.com>
2017-04-05 15:35:33 +0300

    ticket:5 add photo field in model, change base class

--- apps/profile/models.py
+++ apps/profile/models.py
@@ -3,18 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+from PIL import Image
 
 
-class Profile(models.Model):
+class Profile(User):
     """
     Model represents user's profile
     """
 
-    user = models.ForeignKey(
-        User,
-        related_name="profile",
-        on_delete=models.CASCADE
-    )
     birthday = models.DateField(
         verbose_name=_("Date of birth"),
         blank=True, null=True
@@ -36,10 +34,25 @@ class Profile(models.Model):
         verbose_name=_('Other contacts'),
         null=True, blank=True
     )
+    photo = models.ImageField(
+        verbose_name=_('Photo'),
+        upload_to='photo/',
+        null=True, blank=True
+    )
 
     class Meta:
         verbose_name = _("Profile")
         verbose_name_plural = _("Profiles")
 
     def __unicode__(self):
-        return "{0} {1}".format(self.user.first_name, self.user.last_name)
+        return "{0} {1}".format(self.first_name, self.last_name)
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        if self.photo:
+            size = settings.PHOTO_DIMENSIONS_WIDTH,\
+                   settings.PHOTO_DIMENSIONS_HIGHT
+            image = Image.open(self.photo.path)
+            image.thumbnail(size, Image.ANTIALIAS)
+            image.save(self.photo.path)
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 admin.autodiscover()
@@ -9,3 +11,5 @@ urlpatterns = patterns(
     url(r'^', include('apps.profile.urls', namespace="profile")),
     url(r'^requests/', include('apps.httplog.urls', namespace="httplog")),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
