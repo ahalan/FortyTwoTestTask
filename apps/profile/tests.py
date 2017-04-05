8cbdc990a449e5021bed42dbbbca26494f296e92
Andrey Halan <halan.andrey@gmail.com>
2017-04-05 15:35:58 +0300

    ticket:5 added edit profile view, tests

--- apps/profile/tests.py
+++ apps/profile/tests.py
@@ -1,27 +1,24 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
 from django.core import management
 from django.core.urlresolvers import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
 from django.test import TestCase
 from django.test.client import Client
 
 from apps.profile.models import Profile
+from apps.profile.forms import ProfileEditForm
 
-USER_DATA = {
-    "id": 1,
+
+PROFILE_DATA = {
     "first_name": "Andrey",
     "last_name": "Halan",
     "email": "halan.andrey@gmail.com",
-    "username": "ahalan"
-}
-
-PROFILE_DATA = {
-    "user_id": USER_DATA['id'],
+    "username": "ahalan",
     "birthday": "1993-09-09",
     "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
             Phasellus vel diam quis libero dignissim ullamcorper.",
-
     "skype": "firestarter--",
     "jabber": "halan.andrey@42cc.co",
     "other_contacts": "ETC",
@@ -39,15 +36,8 @@ class ViewsTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create(**USER_DATA)
 
-    def test_failed_homepage(self):
-        """ Test homepage view when profile does not exists """
-
-        response = self.client.get(reverse('profile:home'))
-        self.assertEqual(response.status_code, 404)
-
-    def test_homepage(self):
+    def test_home_page(self):
         """ Test homepage view when profile exists """
 
         Profile.objects.create(**PROFILE_DATA)
@@ -55,6 +45,26 @@ class ViewsTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'profile.html')
+        self.assertTrue('<!DOCTYPE html>' in response.content)
+
+    def test_get_edit_page(self):
+        """ Test profile edit view on get request """
+
+        Profile.objects.create(**PROFILE_DATA)
+        response = self.client.get(reverse('profile:edit'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit.html')
+        self.assertTrue('<!DOCTYPE html>' in response.content)
+
+    def test_post_edit_page(self):
+        """ Test profile edit view on post request """
+
+        Profile.objects.create(**PROFILE_DATA)
+        response = self.client.get(reverse('profile:edit'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit.html')
         self.assertTrue('<!DOCTYPE html>' in response.content)
 
 
@@ -67,11 +77,45 @@ class ModelTest(TestCase):
             'flush', interactive=False, load_initial_data=False
         )
 
-    def setUp(self):
-        self.user = User.objects.create(**USER_DATA)
-
     def test_profile_creation(self):
         """ Test profile instance creation """
 
         Profile.objects.create(**PROFILE_DATA)
         self.assertEquals(Profile.objects.count(), 1)
+
+    def test_image_resize(self):
+        """ Tests if submited image gets resized """
+
+        profile = Profile.objects.create(**PROFILE_DATA)
+        profile.photo = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=open('assets/img/empty.jpg', 'rb').read(),
+            content_type='image/jpeg')
+        profile.save()
+
+        self.assertEqual(settings.PHOTO_DIMENSIONS_HIGHT, profile.photo.height)
+        self.assertEqual(settings.PHOTO_DIMENSIONS_WIDTH, profile.photo.width)
+
+
+class FormsTest(TestCase):
+    """ Tests for profile form """
+
+    @classmethod
+    def setUpClass(cls):
+        management.call_command(
+            'flush', interactive=False, load_initial_data=False
+        )
+
+    def setUp(self):
+        self.profile = Profile.objects.create(**PROFILE_DATA)
+
+    def test_edit_event(self):
+        data = PROFILE_DATA
+        data['first_name'] = 'test'
+        data['biography'] = 'biography test'
+        data['skype'] = 'skype test'
+        data['jabber'] = 'jabber@test.com'
+
+        form = ProfileEditForm(data, instance=self.profile)
+        form.is_valid()
+        self.assertTrue(form.is_valid())
--- apps/profile/views.py
+++ apps/profile/views.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+import json
+
+from django.shortcuts import render
 from django.views.generic.base import View
+from django.http import HttpResponse
 
 from apps.profile.models import Profile
 from apps.profile.forms import ProfileEditForm
+
 
 class ProfileHomeView(View):
     """ Class based view for home page """
@@ -12,14 +16,34 @@ class ProfileHomeView(View):
     template_name = "profile.html"
 
     def get(self, request):
-        profile = get_object_or_404(Profile, id=1)
+        profile = Profile.objects.first()
         return render(request, self.template_name, {'profile': profile})
 
 
 class ProfileEditView(View):
-    """ Class based view for home page """
+    """ Class based view editing profile """
 
     template_name = "edit.html"
+    form_class = ProfileEditForm
 
     def get(self, request):
-        return render(request, self.template_name, {'form': ProfileEditForm()})
+        form = self.form_class(instance=Profile.objects.first())
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        response_data = dict(success=True, payload={})
+        form = self.form_class(
+            request.POST,
+            request.FILES,
+            instance=Profile.objects.first()
+        )
+        if form.is_valid():
+            profile = form.save()
+            response_data['payload']['photo_url'] = profile.photo.url
+        else:
+            response_data['success'] = False
+            response_data['payload']['errors'] = dict(form.errors.items())
+
+        return HttpResponse(json.dumps(response_data),
+                            content_type="application/json")
+
