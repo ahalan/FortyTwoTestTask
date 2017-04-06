2829b7f89eaa00772dd2b36443bcefc4cba2de6f
Andrey Halan <halan.andrey@gmail.com>
2017-04-06 13:16:38 +0300

    ticket:3 updated view

--- apps/httplog/views.py
+++ apps/httplog/views.py
@@ -14,12 +14,24 @@ class RequestsHistoryView(View):
     template_name = "block/requests_list.html"
 
     def get(self, request):
+        def is_non_viewed(entry, last_id):
+            entry.non_viewed = entry.id > last_id
+            return entry
+
         if request.is_ajax():
-            entries = HttpRequestEntry.objects.all()
-            context = {
-                'entries': entries[:settings.HTTP_LOG_ENTRIES_ON_PAGE]
-            }
-            return render(request, self.template_name, context)
+            last_entry_id = HttpRequestEntry.objects.first().id
+            last_viewed_id = request.session.get('last_viewed_id')
+            entries = HttpRequestEntry.objects.all()[
+                      :settings.HTTP_LOG_ENTRIES_ON_PAGE]
+
+            entries = [is_non_viewed(e, last_viewed_id) for e in entries]
+            new_entries_count =  len([e for e in entries if e.non_viewed])
+            request.session['last_viewed_id'] = last_entry_id
+
+            return render(request, self.template_name, {
+                'entries': entries,
+                'non_viewed_count': new_entries_count
+            })
         return HttpResponseBadRequest()
 
 
