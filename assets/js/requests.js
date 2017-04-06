2ab1885c3bbc9bbb30a265289b6b7a0391b45b1c
Andrey Halan <halan.andrey@gmail.com>
2017-04-06 13:16:16 +0300

    ticket:3 added request counter to page title

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,18 +1,28 @@
 var requests = {
     requestsBlock: $(".request-history"),
+    nonViewedCountId: "#non_viewed_count",
 
     init: function (url) {
         var self = this;
         self.url = url;
 
-        self.getRequestHistory(self.url);
-        setTimeout(function () {
-            self.getRequestHistory(self.url)
-        }, 10000);
+        self.getRequestHistory(self);
     },
-    getRequestHistory: function (url) {
-        $.get(url, function (data) {
-            $(self.requestsBlock).html(data);
+
+    getRequestHistory: function (self) {
+        $.get(self.url, function (data) {
+            self.requestsBlock.html(data);
+
+            var counter = $(self.nonViewedCountId).val();
+            if (counter != 0) {
+                document.title = "Requests (" + counter + ")";
+            } else {
+                document.title = "Requests";
+            }
+
+            setTimeout(function () {
+                self.getRequestHistory(self);
+            }, 5000);
         });
     }
 }
