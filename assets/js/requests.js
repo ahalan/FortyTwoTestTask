ff0d08ed2c9351ecc25e581f9a4cc355edc7d2f5
Andrey Halan <halan.andrey@gmail.com>
2017-04-10 22:43:16 +0300

    ticket:13 updated requests scripts on priority

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,13 +1,17 @@
 var requests = {
     requestsBlock: $(".request-history"),
-    nonViewedCountId: "#non_viewed_count",
 
-    init: function (url) {
+    tableBodySelector: ".table tbody",
+    tableRowSelector: ".table tbody tr",
+    nonViewedSelector: "#non_viewed_count",
+
+    init: function (opts) {
         var self = this;
 
-        self.history_url = url;
-        self.is_first_init = true;
-        self.title = 'Requests';
+        self.title = opts.title;
+        self.history_url = opts.url;
+        self.is_init = true;
+        self.interval = 0;
 
         self.isFocused(self);
         self.getRequestHistory(self);
@@ -21,8 +25,8 @@ var requests = {
         self.timeout = 1000;
         self.url = self.history_url;
 
-        if (self.is_first_init) {
-            self.is_first_init = false;
+        if (self.is_init) {
+            self.is_init = false;
         } else {
             if (!document.hidden) {
                 self.url = self.history_url + "?viewed=true";
@@ -32,21 +36,66 @@ var requests = {
         }
     },
 
+    initSortable: function (self) {
+        var fixHelper = function (e, ui) {
+            ui.children().each(function () {
+                $(this).width($(this).width());
+            });
+            return ui;
+        };
+
+        $(self.tableBodySelector).sortable({
+            update: function (event, ui) {
+                var entries = [];
+
+                // clearInterval(self.interval);
+
+                $(self.tableRowSelector).each(function () {
+                    var $firstChild = $(this).children('td:first-child');
+
+                    $firstChild.html($(this).index());
+
+                    entries.push({
+                        id: $(this).attr('data-id'),
+                        priority: $firstChild.text()
+                    })
+                });
+
+                $.ajax({
+                    type: "POST",
+                    url: self.history_url,
+                    data: {
+                        'entries': JSON.stringify(entries)
+                    },
+                    dataType: 'json',
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                    }
+                });
+            },
+            helper: fixHelper
+        }).disableSelection();
+
+    },
+
     getRequestHistory: function (self) {
         $.get(self.url, function (data) {
             self.requestsBlock.html(data);
 
             // Update page title with (n) new requests
-            var counter = $(self.nonViewedCountId).val();
+            var counter = $(self.nonViewedSelector).val();
             if (counter != 0) {
                 document.title = self.title + " (" + counter + ")";
             } else {
                 document.title = self.title;
             }
 
-            setTimeout(function () {
+            self.initSortable(self);
+
+            self.interval = setInterval(function () {
                 self.getRequestHistory(self);
             }, self.timeout);
+
         });
     }
-}
+};
