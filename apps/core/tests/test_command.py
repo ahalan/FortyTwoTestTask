256c30d19bd6ff24e638a46529f327dd34b709a1
Andrey Halan <halan.andrey@gmail.com>
2017-04-12 15:56:19 +0300

    ticket:8 updated command tests

--- apps/core/tests/test_command.py
+++ apps/core/tests/test_command.py
@@ -4,21 +4,30 @@ import sys
 
 from django.test import TestCase
 from django.core import management
+from django.db import models
+
 from StringIO import StringIO
 
 
-class CommandTest(TestCase):
+class CoreCommandTest(TestCase):
     """ Test for core commands """
+
+    def setUp(self):
+        self.models = models.get_models(include_auto_created=True)
 
     def test_get_model_list_command(self):
         """ Test for core command """
 
         out = StringIO()
         saved_stderr = sys.stderr
+        sys.stderr = out
 
-        try:
-            sys.stderr = out
-            management.call_command('get_models_list')
-            self.assertIn('error:', out.getvalue())
-        finally:
-            sys.stderr = saved_stderr
+        management.call_command('get_models_list')
+
+        for model in self.models:
+            info = 'error: model - {model}; count - {count}\n'.format(
+                model=model.__name__,
+                count=model.objects.count())
+            self.assertIn(info, out.getvalue())
+
+        sys.stderr = saved_stderr
