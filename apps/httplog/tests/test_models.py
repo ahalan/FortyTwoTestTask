c5a27d46b29a32fc78223c54f4d19d1dfbb97104
Andrey Halan <halan.andrey@gmail.com>
2017-04-28 11:15:26 +0300

    ticket:3 added test for model failed creation

--- apps/httplog/tests/test_models.py
+++ apps/httplog/tests/test_models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.db import IntegrityError
 
 from apps.httplog.models import HttpRequestEntry
 
@@ -18,3 +19,16 @@ class HttpRequestEntryModelTest(TestCase):
             status_code=200
         )
         self.assertEquals(HttpRequestEntry.objects.count(), 1)
+
+    def test_httplog_entry_creation_failed(self):
+        """ Test httplog instance creation failed """
+        with self.assertRaises(IntegrityError) as context:
+            HttpRequestEntry.objects.create(
+                method='GET',
+                host='localhost',
+                path='/',
+            )
+
+            error_msg = context.exception.message
+            self.assertContains('status_code', error_msg)
+            self.assertContains('NOT NULL constraint failed', error_msg)
