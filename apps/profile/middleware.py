dfc1956288e7ad0d0dc9a105f6edbb5a569895f7
Andrey Halan <halan.andrey@gmail.com>
2017-05-13 16:23:12 +0300

    ticket:17 debug error

--- apps/profile/middleware.py
+++ apps/profile/middleware.py
@@ -17,7 +17,8 @@ class GeolocationMiddleware(object):
 
             try:
                 latlng = GeoIP().coords(client_ip)
-            except:
+            except Exception as e:
+                print e
                 latlng = None
 
             request.user.lng, request.user.lat = latlng or (None, None)
