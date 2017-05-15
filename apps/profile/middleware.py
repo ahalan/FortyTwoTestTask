7c894ea39d3629651458d2f3e67b5e2dfbfedefe
Andrey Halan <halan.andrey@gmail.com>
2017-05-15 17:12:20 +0300

    ticket:17 removed debug prints

--- apps/profile/middleware.py
+++ apps/profile/middleware.py
@@ -19,14 +19,7 @@ class GeolocationMiddleware(object):
                 client_ip = request.META.get('REMOTE_ADDR')
 
             try:
-                g = GeoIP(path=settings.GEOIP_PATH)
-                print settings.GEOIP_PATH, os.path.isdir(settings.GEOIP_PATH)
-                print "City: ", settings.GEOIP_CITY, g._city, g._city_file,\
-                    os.path.isfile(g._city_file)
-                print "Country: ", settings.GEOIP_COUNTRY, g._country,\
-                    g._country_file, os.path.isfile(g._country_file)
-
-                latlng = g.coords(client_ip)
+                latlng = GeoIP().coords(client_ip)
             except Exception as e:
                 print "Error: %s" % e
                 latlng = None
