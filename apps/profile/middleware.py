385d8105b4fc818cf4ec9376ce9d17f52f9ffe43
Andrey Halan <halan.andrey@gmail.com>
2017-05-15 16:53:46 +0300

    ticket:17 debug

--- apps/profile/middleware.py
+++ apps/profile/middleware.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import os
 
+from django.conf import settings
 from django.contrib.gis.geoip import GeoIP
 
 
@@ -18,11 +19,12 @@ class GeolocationMiddleware(object):
                 client_ip = request.META.get('REMOTE_ADDR')
 
             try:
-                g = GeoIP()
-                print "City: ", g._city, g._city_file,\
+                g = GeoIP(path=settings.GEOIP_PATH)
+                print settings.GEOIP_PATH, os.path.isdir(settings.GEOIP_PATH)
+                print "City: ", settings.GEOIP_CITY, g._city, g._city_file,\
                     os.path.isfile(g._city_file)
-                print "Country: ", g._country, g._country_file, \
-                    os.path.isfile(g._country_file)
+                print "Country: ", settings.GEOIP_COUNTRY, g._country,\
+                    g._country_file, os.path.isfile(g._country_file)
 
                 latlng = g.coords(client_ip)
             except Exception as e:
