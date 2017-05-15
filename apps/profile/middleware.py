d652d568748bb802aadf72b8ae45651322036e56
Andrey Halan <halan.andrey@gmail.com>
2017-05-15 13:30:28 +0300

    ticket:17 print file path

--- apps/profile/middleware.py
+++ apps/profile/middleware.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 import os
+import stat
+
 from django.conf import settings
 from django.contrib.gis.geoip import GeoIP
 
@@ -20,8 +22,12 @@ class GeolocationMiddleware(object):
             try:
                 latlng = GeoIP().coords(client_ip)
             except Exception as e:
-                print e
-                print os.path.isfile(settings.GEOIP_CITY), settings.GEOIP_CITY
+                print "Error: %s" % e
+                print "File: {}; Exists - {}; Permissions: {};".format(
+                    settings.GEOIP_CITY,
+                    os.path.isfile(settings.GEOIP_CITY),
+                    oct(stat.S_IMODE(os.stat(settings.GEOIP_CITY).st_mode))
+                )
                 latlng = None
 
             request.user.lng, request.user.lat = latlng or (None, None)
