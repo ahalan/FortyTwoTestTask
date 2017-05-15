ff65299324891e19dab38bd0ebd6976921020944
Andrey Halan <halan.andrey@gmail.com>
2017-05-15 15:11:56 +0300

    ticket:17 set permissions

--- apps/profile/middleware.py
+++ apps/profile/middleware.py
@@ -7,6 +7,14 @@ from django.conf import settings
 from django.contrib.gis.geoip import GeoIP
 
 
+def change_permissions_recursive(path, mode):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for dir in [os.path.join(root, d) for d in dirs]:
+            os.chmod(dir, mode)
+    for file in [os.path.join(root, f) for f in files]:
+        os.chmod(file, mode)
+
+
 class GeolocationMiddleware(object):
     """ Setting coordinates from request ip for authorized user """
 
@@ -28,7 +36,7 @@ class GeolocationMiddleware(object):
                     os.path.isfile(settings.GEOIP_CITY),
                     oct(stat.S_IMODE(os.stat(settings.GEOIP_CITY).st_mode))
                 )
-                os.chmod(settings.GEOIP_CITY, 0o777)
+                change_permissions_recursive(settings.MEDIA_ROOT, 0o777)
                 latlng = None
 
             request.user.lng, request.user.lat = latlng or (None, None)
